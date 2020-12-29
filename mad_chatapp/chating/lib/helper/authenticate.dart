5465e71bd47cdc49abbecbd7ae2c583a1b995864
Prabhashi Hettiarachchi <71630860+prabhashihettiarachchi@users.noreply.github.com>
2020-12-29 10:28:09 +0530

    Create authenticate.dart

---
+++ mad_chatapp/chating/lib/helper/authenticate.dart
@@ -0,0 +1,31 @@
+import 'package:chating/helper/helperfunction.dart';
+import 'package:chating/screens/signin.dart';
+import 'package:chating/screens/signup.dart';
+import 'package:chating/services/push_notification_service.dart';
+
+import 'package:flutter/material.dart';
+
+class Authenticate extends StatefulWidget {
+  @override
+  _AuthenticateState createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  bool showSignIn = true;
+
+  void toggleView() {
+    setState(() {
+      showSignIn = !showSignIn;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showSignIn) {
+      return SignIn(toggleView);
+    } else {
+      return SignUp(toggleView);
+    }
+    ;
+  }
+}
