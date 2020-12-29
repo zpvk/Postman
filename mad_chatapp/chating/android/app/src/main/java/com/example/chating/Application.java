edee439068592989bf0142ade36c468bbbfc9f98
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-12-29 09:55:45 +0530

    Final updated Structure
    This content only app structure not included main  files

---
+++ mad_chatapp/chating/android/app/src/main/java/com/example/chating/Application.java
@@ -0,0 +1,27 @@
+/*
+ * @Author: Rohan Kumara
+ * @Date:   2020-12-28 03:49:18
+ * @Last Modified by:   Rohan Kumara
+ * @Last Modified time: 2020-12-28 03:50:12
+ */
+package io.flutter.plugins.pusnnotification;
+
+import io.flutter.app.FlutterApplication;
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin;
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
+
+public class Application extends FlutterApplication implements PluginRegistrantCallback {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FlutterFirebaseMessagingService.setPluginRegistrant(this);
+    }
+
+    @Override
+    public void registerWith(PluginRegistry registry) {
+        FirebaseMessagingPlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"));
+    }
+}
---
+++ mad_chatapp/chating/android/app/src/main/java/com/example/chating/MainActivity.java
@@ -0,0 +1,13 @@
+/*
+ * @Author: Rohan Kumara
+ * @Date:   2020-12-28 03:26:11
+ * @Last Modified by:   Rohan Kumara
+ * @Last Modified time: 2020-12-28 03:26:11
+ */
+package com.example.chating;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
+
